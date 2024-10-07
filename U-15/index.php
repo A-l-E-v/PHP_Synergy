7877b3afbf46c6a16038a106ad4ee006b1d5295f
Al Ev <alexey_e@mail.ru>
2024-10-07 19:43:01 +0300

    tasks are done! git links

---
+++ U-15/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$people = array(
+    array ('1','Антонов Игорь Владимирович', 'helloworld@mail.ru', 'муж', '1986'),
+    array('2','Иванова Кристина Викторовна', 'helloworld@mail.ru', 'жен', '1972'),
+    array ('3','Борисов Максим Анатольевич', 'helloworld@mail.ru', 'муж', '1989')
+);
+$rows = count ($people);
+$cols = count($people[0]);
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+<meta charset="utf-8">
+<title>Урок 15</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1><?php echo "Привет, Новый пользователь!";?></h1>
+    <h3><?php echo date('d-m-Y');?></h3>
+<table class="table">
+    <thead>
+        <th>№</th>
+        <th>ФИО</th>
+        <th>E-mail</th>
+        <th>Пол</th>
+        <th>Года рождения</th>
+    </thead>
+    <?php
+    echo "<tbody>";
+    foreach ($people as $row){
+       echo "<tr>";     
+        foreach ($row as $col){echo "<td>".$col."</td>";}
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    ?>
+</table>
+</body>
