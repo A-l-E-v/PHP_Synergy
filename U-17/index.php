20082cb8ff45a55b6cf4cd8b823ad59a2bcf6c3e
Al Ev <alexey_e@mail.ru>
2024-10-08 07:36:23 +0300

    docx is done!

--- U-17/index.php
+++ U-17/index.php
@@ -1,5 +1,7 @@
 <?php
 
+// https://github.com/A-l-E-v/PHP_Synergy/blob/main/U-17/index.php
+
 // 1. Необходимо организовать передачу следующего содержимого “Это первая строка” 
 // из буфера в переменную при помощи функции ob_get_contents() и в дальнейшем 
 // осуществить вывод этой переменной, помните, что любой пользовательский буфер 
@@ -23,3 +25,56 @@ ob_end_clean();
 echo "Это вторая строка <br>";
 echo $firstLine;
 echo ("<br>");
+
+
+// 3. Нужно написать свою callback функцию которая перевернет строчку “reverse string”
+
+// callback функция разворота строки
+function reverse($string){
+    return strrev($string);
+}
+
+// запускаем буфер с callback-функцией разворота строки
+ob_start("reverse");
+// выводим строку в буфер
+echo "reverse string";
+// завершаем работу с буфером со сбросом в браузер
+ob_end_flush();
+
+echo ("<br>");
+ob_end_flush();
+
+
+// 4. Вам необходимо внести изменения в представленный код и изменить последовательность символов, 
+// так чтобы выводимые значения ранжировались по убыванию
+// в результате вы должны получить подобный результат:
+// Это исходный код.
+// 0123
+// Это итоговый код.
+// 3210
+
+
+echo "Это исходный код.<br>";
+echo ob_get_level();
+ob_start();
+    echo ob_get_level();
+    ob_start();
+        echo ob_get_level();
+        ob_start();
+            echo ob_get_level();
+        ob_end_flush();
+    ob_end_flush();
+ob_end_flush();
+echo "<br>";
+
+echo "Это итоговый код.<br>";
+ob_start();
+    ob_start();
+        ob_start();
+            echo ob_get_level();
+        ob_end_flush();
+    echo ob_get_level();
+    ob_end_flush();
+echo ob_get_level();
+ob_end_flush();
+echo ob_get_level();
